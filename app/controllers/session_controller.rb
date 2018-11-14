67b193913e3d346b9d467efc382487feb9f195d9
Mai Hữu Phúc <huuphuc.ns@gmail.com>
2018-11-14 08:33:06 +0700

    role

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,7 +2,7 @@ class SessionController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
+    user = User.find_by user_name: params[:session][:user_name].downcase
     if user&.authenticate params[:session][:password]
       log_in user
       redirect_to user
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,20 +4,29 @@ class UserController < ApplicationController
   end
 
   def create
+    # binding.pry
     @user = User.new user_params
     if @user.save
       log_in @user
       flash[:success] = t ".Sign_Up_Success"
-      redirect_to @user
+      redirect_to "/login"
     else
       flash[:error] = t ".registration_failed"
       render :new
     end
+    # redirect_to "/login"
   end
 
   private
 
   def user_params
-    params.require(:user).permit :name, :email, :password, :confirm
+    params.require(:user).permit :user_name, :email, :password, :confirm
+  end
+
+  def logged_in_user
+      return if logged_in?
+      store_location
+      flash[:danger] = t "please_login"
+      redirect_to login_url
   end
 end
